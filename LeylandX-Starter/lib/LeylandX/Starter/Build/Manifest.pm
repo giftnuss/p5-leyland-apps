98c935e3e6a171261c7a6e04abb02eaf16618fed
giftnuss <rock@ccls-online.de>
2011-06-18 01:25:57 +0200

    fix: manifest depends on more

---
+++ LeylandX-Starter/lib/LeylandX/Starter/Build/Manifest.pm
@@ -0,0 +1,45 @@
+  package LeylandX::Starter::Build::Manifest
+# ******************************************
+; our $VERSION = '0.01';
+# **********************
+use Moose;
+
+with 'LeylandX::Starter::Task',
+     'LeylandX::Starter::Task::Builder';
+
+sub command { "$^X Build.PL && ./Build manifest" }
+
+sub depends { qw
+  ( 
+    app
+    basedir 
+    build
+    changes
+    manifest_skip
+    psgi
+    root
+    view_default
+  ) 
+}
+
+sub detect 
+{
+    my ($self) = @_;
+    my $basedir = $self->get_dependency('basedir')->basedir;
+    my $file = Path::Class::File->new($basedir,'MANIFEST');
+
+    return !! $file->stat;
+}
+
+sub forProject
+{
+    my ($self,$project) = @_;
+    return $self->new();
+}
+
+no Moose;
+
+1;
+
+__END__
+
