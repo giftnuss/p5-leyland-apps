64a26bf1bdc42cc3fded4590e33461855b9da72f
giftnuss <rock@ccls-online.de>
2011-09-06 02:05:55 +0200

    disable loggin in tests

--- LeylandX-Starter/lib/LeylandX/Starter/Build/Psgi.pm
+++ LeylandX-Starter/lib/LeylandX/Starter/Build/Psgi.pm
@@ -27,6 +27,76 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
 
-__END__
+__DATA__
 
+____[ app.psgi ]____________________________________________________________
+#!/usr/bin/perl -w
 
+use lib './lib';
+use strict;
+use warnings;
+use [== $package_name =];
+use Plack::Builder;
+
+my $config = {
+	app => '[== $package_name =]',
+	views => ['Tenjin'],
+	locales => './i18n',
+	logger => {
+		class => 'LogHandler',
+		opts => {
+			outputs => [
+				file => {
+					filename => "[== lc($app_name) =].$ENV{PLACK_ENV}.log",
+					minlevel => 0,
+					maxlevel => 8,
+					utf8 => 1,
+				},
+				screen => {
+					log_to   => "STDERR",
+					minlevel => 0,
+					maxlevel => 8,
+				},
+			]
+		}
+	},
+	environments => {
+		development => {
+			# options in here will override top level options when running in the development environment
+		},
+		deployment => {
+			# options in here will override top level options when running in the deployment environment
+		},
+                testing => {
+                     # keep console clean during unit tests
+		     logger => {
+                         class => 'LogHandler',
+                         opts => {
+			     outputs => [
+			         screen => { log_to => "STDERR", minlevel => 0, maxlevel => 4}
+                             ]
+                         }
+                     }
+                }
+	}
+};
+
+my $a = [== $package_name =]->new(config => $config);
+
+my $app = sub {
+	$a->handle(shift);
+};
+
+builder {
+	# enable whatever Plack middlewares you wish here, a good example
+	# would be the Session middleware.
+	# --------------------------------------------------------------
+	# the Static middleware will serve static files from the app's
+	# public directory, remove it (or comment it) if your web server
+	# is serving those files
+	enable 'Static',
+		path => qr{^/((images|js|css|fonts)/|favicon\.ico$|apple-touch-icon\.png$)},
+		root => './public/';
+
+	$app;
+};
