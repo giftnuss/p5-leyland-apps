4342dec269a01fe1a731d4f5201cf685b1c4f1bc
Sebatian B. Knapp <rock@ccls-online.de>
2011-12-19 21:12:15 +0100

    start a new app every month

---
+++ Satz/app.psgi
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+
+use lib './lib';
+use strict;
+use warnings;
+use Satz;
+use Plack::Builder;
+
+my $config = {
+	app => 'Satz',
+	views => ['Tenjin'],
+	locales => './i18n',
+	logger => {
+		class => 'LogHandler',
+		opts => {
+			outputs => [
+				file => {
+					filename => "satz.$ENV{PLACK_ENV}.log",
+					minlevel => 0,
+					maxlevel => 8,
+					utf8 => 1,
+				},
+				screen => {
+					log_to   => "STDERR",
+					minlevel => 0,
+					maxlevel => 8,
+				},
+			]
+		}
+	},
+	environments => {
+		development => {
+			# options in here will override top level options when running in the development environment
+		},
+		deployment => {
+			# options in here will override top level options when running in the deployment environment
+		},
+	}
+};
+
+my $a = Satz->new(config => $config);
+
+my $app = sub {
+	$a->handle(shift);
+};
+
+builder {
+	# enable whatever Plack middlewares you wish here, a good example
+	# would be the Session middleware.
+	# --------------------------------------------------------------
+	# the Static middleware will serve static files from the app's
+	# public directory, remove it (or comment it) if your web server
+	# is serving those files
+	enable 'Static',
+		path => qr{^/((images|js|css|fonts)/|favicon\.ico$|apple-touch-icon\.png$)},
+		root => './public/';
+
+	$app;
+};
---
+++ Satz/lib/LeylandX/Plugin/Form.pm
@@ -0,0 +1,76 @@
+  package LeylandX::Plugin::Form;
+# *******************************
+  our $VERSION = '0.01';
+# **********************
+use Moose::Role;
+use Module::Pluggable;
+
+requires 'app';
+
+has form_ns =>
+(
+    is => 'rw',
+    isa => 'Str',
+    lazy => 1,
+    default => sub {
+        my ($self) = @_;
+        blessed( $self->app ) . '::Form';
+    }
+);
+
+has forms =>
+(
+    is => 'ro',
+    isa => 'HashRef',
+    lazy => 1,
+    builder => 'load_forms',
+    predicate => 'has_forms'
+);
+
+sub load_forms {
+    my ($self,$namespace) = @_;
+    $namespace ||= $self->form_ns;
+    my $forms = $self->has_forms ? $self->forms : {};
+
+    my $finder = Module::Pluggable::Object->new(
+            search_path => [$namespace],
+            require => 1
+    );
+
+    foreach my $formclass ($finder->plugins) {
+        my $name = lc(substr($formclass,length($namespace)));
+        $name =~ s/^:://;
+        $name =~ s/::/-/;
+        $forms->{$name} = $formclass;
+    }
+    return $forms;
+}
+
+sub form {
+    my ($self,$name,@args) = @_;
+    my $forms = $self->forms;
+    return undef unless defined $forms->{$name};
+    return $forms->{$name} if blessed $forms->{$name};
+    $forms->{$name} = $forms->{$name}->new(@args);
+
+    # in leyland it is common to give the current context to the form
+    # for further usage.
+    if( $forms->{$name}->can('context') ) {
+        $forms->{$name}->context($self);
+    }
+    return $forms->{$name}
+}
+
+no Moose::Role;
+1;
+
+__END__
+
+=head1 NAME
+
+LeylandX::Plugin::Form - create form objects by name
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
